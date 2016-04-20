cb255ae64574137113f6d5e844c84c10f6895630
Fatima Rafiqui <fatima.rafiqui@gmail.com>
2016-04-20 22:13:25 +0530

    Create CheckForComments.c

---
+++ CheckForComments.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char com[30];
+	int i=2,a=0;
+	printf (" Enter comment: \n "); 
+	gets(com);
+	if(com[0]=='/') 
+	{ 
+		if(com[1]=='/')
+			printf("It is a comment \n");
+		else if(com[1]=='*')
+		{
+			for(i=2;i<=30;i++) 
+			{
+				if(com[i]=='*' && com[i+1]=='/') 
+				{
+					printf("It is a comment \n "); 
+					a=1;
+					break;
+				}
+				else
+				continue;
+			}
+			if(a==0)
+			printf("It is not a comment \n ");
+		}
+		else
+		printf(" It is not a comment \n "); 
+	}
+	else
+	printf(" It is not a comment \n ");
+	return 0;
+}
